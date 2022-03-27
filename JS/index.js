0ad92dc4fdcc973591a4ac5180d39771e938f163
omar-su <73144494+Omar-su@users.noreply.github.com>
2022-03-27 17:08:55 +0200

    changed my approach for the website, decided to make a more game like portfolio

--- JS/index.js
+++ JS/index.js
@@ -1,86 +1,179 @@
-$(document).ready(function(){
 
-// Variables
-  $highWay = $('.carHighWay');
-  $car = $('.car');
-  $img = $('.car img');
-  let flag = true;
-  var x = 0;
-  var y = 0;
-  var z = 0;
-  var x_highWay = 0;
-  var y_highWay = 0;
+const canvas = document.querySelector('canvas')
 
+const c = canvas.getContext('2d')
 
+canvas.width = innerWidth
+canvas.height = innerHeight
 
-const cars =['../assets/assets/Img_05.png','../assets/assets/Img_06.png' ] 
+console.log(c)
+const gravity = 0.5
 
-// Keypressed event
-$(document).on('keypress',function(e){
+// Player class 
+class Player{
 
-
-
-  if(e.which == 100){
-    x = x + 10
-    x_highWay = x_highWay -10;
-    z = anim(x,z, x_highWay)
-
-  }
-  if(e.which == 97){
-    x = x - 10
-    z = anim(x,z, x_highWay)
-
+ 
+  constructor(){
+    this.position = {
+      x: 100,
+      y: 100
+    }
+    this.velocity =  {
+      x: 0,
+      y: 0
+    }
+    this.width = 50;
+    this.height = 50;
   }
 
+  draw(){
+    c.fillStyle = 'red'
+    c.fillRect(this.position.x, this.position.y, this.width, this.height)
+  }
 
-})
+  update(){
+    this.draw()
 
-function anim(x, z, x_highWay){
-if(z == 0){
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+    
+    if(this.position.y + this.height + this.velocity.y<= canvas.height){
+      this.velocity.y += gravity
+    }
+    
+    else {this.velocity.y = 0}
+      
+  }
 
-  console.log(z);
-  $($car).css({"transform":"translate("+x + "px," + -1 + "px)" });
-  $($highWay).css({"transform":"translateX("+ x_highWay + "px)" });
-  z = 1;
-}else if(z==1){
-  console.log(z);
-
-  $($car).css({"transform":"translate("+x + "px," + 2 + "px)" });
-  $($highWay).css({"transform":"translateX("+ x_highWay + "px)" });
-  z = 2;
-}else if(z==2){
-  console.log(z);
-
-  $($car).css({"transform":"translate("+x + "px," + -1 + "px)" });
-  $($highWay).css({"transform":"translateX("+ x_highWay + "px)" });
-  z = 0;
-}
-return z;
 
 }
 
-$(document).on('keypress',function(e){
+class Platform{
+  constructor({x,y}){
+    this.position = {
+      x,
+      y
+    }
+    this.width = 200
+    this.height = 20
+  }
 
-  if(e.which == 119){
-    if(flag){
-      flag = false;
-      $img.attr('src', cars[0]);
-    }else{
-      flag = true;
-      $img.attr('src', cars[1]);
+  draw(){
+    c.fillStyle = 'blue'
+    console.log('hello')
+    c.fillRect(this.position.x, this.position.y, this.width, this.height)
+  }
+
+}
+
+
+
+
+const player = new Player()
+const platforms = [new Platform({x:100, y:200}), new Platform({x:500,y:200})]
+
+
+
+const keys = {
+  right: {
+    pressed: false
+  },
+  left: {
+    pressed: false
+  }
+}
+
+
+function animate(){
+  requestAnimationFrame(animate)
+  c.clearRect(0,0, canvas.width, canvas.height)
+  player.update()
+  platforms.forEach((platform) => {
+    platform.draw()
+  })
+
+  if(keys.right.pressed && player.position.x < 400){
+    player.velocity.x = 5
+  }else if(keys.left.pressed && player.position.x > 100){
+    player.velocity.x = -5
+  }else{
+    player.velocity.x = 0
+
+    if(keys.right.pressed){
+      platforms.forEach((platform) => {
+        platform.position.x -= 5
+      })
+      
+    }else if(keys.left.pressed){
+      platforms.forEach((platform) => {
+        platform.position.x += 5
+      })
+      
     }
 
+
   }
+  platforms.forEach((platform) => {
+    if(player.position.y + player.height <= platform.position.y && player.position.y + player.height+ player.velocity.y >= platform.position.y && player.position.x + player.width >= platform.position.x && player.position.x <= platform.position.x + platform.width){
+        player.velocity.y = 0
+    }
+
+  })
+  
+
+}
+
+animate()
+
+window.addEventListener('keydown', ({keyCode}) => {
+    switch(keyCode){
+      //left
+      case 65:
+        keys.left.pressed = true
+
+        break
+      // down
+      case 83:
+
+        break
+        //right
+      case 68:
+        keys.right.pressed = true
+        break
+      // Jumping
+      case 87:
+
+        player.velocity.y -= 10
+      
+        
+        break
+    
+    
+    }
 })
 
 
+window.addEventListener('keyup', ({keyCode}) => {
+  switch(keyCode){
+    case 65:
+      keys.left.pressed = false
 
+      break
 
+    case 83:
 
-});
+      break
 
+    case 68:
+      keys.right.pressed = false
 
-
-
-
+      break
+    // Jumping
+    case 87:
+      player.velocity.y = 0
+      break
+  
+  
+  }
+})
 
